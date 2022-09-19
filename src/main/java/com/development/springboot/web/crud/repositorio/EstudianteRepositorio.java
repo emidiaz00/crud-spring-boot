5c0fad95057c35717fa2c835e260511f1fa9e72f
emidiaz00 <emildsn00@gmail.com>
2022-09-19 19:52:23 -0300

    agrego servicio e implementacion estudiante

---
+++ src/main/java/com/development/springboot/web/crud/repositorio/EstudianteRepositorio.java
@@ -0,0 +1,8 @@
+package com.development.springboot.web.crud.repositorio;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstudianteRepositorio {
+
+}
---
+++ src/main/java/com/development/springboot/web/crud/servicio/EstudianteServicioImpl.java
@@ -0,0 +1,16 @@
+package com.development.springboot.web.crud.servicio;
+
+import com.development.springboot.web.crud.entidad.Estudiante;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EstudianteServicioImpl implements EstudianteServicio {
+
+
+    @Override
+    public List<Estudiante> listarTodos() {
+        return null;
+    }
+}
